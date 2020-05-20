982c6fdea8e3466a6e7cc34a6aeeb4e66bffe06f
Matthew Alner <matthewalner@googlemail.com>
2020-05-20 17:22:08 +0100

    add more to the kitchen sink

--- kitchenSink/src/index.ts
+++ kitchenSink/src/index.ts
@@ -1,31 +1,101 @@
-import { mocklify, override } from 'mocklify';
+import { mocklify, modify, omit, override, where, Limiter } from 'mocklify';
 
-function prettyPrintUsers(users: IUser[]) {
-  const appDiv: HTMLElement | null = document.getElementById("app");
+enum House {
+  GRYFFINDOR = 'Gryffindor',
+  SLYTHERIN = 'Slytherin'
+}
+
+const isGryffindor: Limiter<IStudent> = student => student.house === House.GRYFFINDOR;
+const isSlytherin: Limiter<IStudent> = student => student.house === House.SLYTHERIN;
+const isHarry: Limiter<IStudent> = student => student.firstName === 'Harry' && student.lastName === 'Potter';
+
+function prettyPrintStudents(students: IStudent[]) {
+  const appDiv: HTMLElement | null = document.getElementById('app');
   if (appDiv) {
-    appDiv.innerHTML = `<pre>${JSON.stringify(users, null, 2)}</pre>`;
+    appDiv.innerHTML = `<pre>${JSON.stringify(students, null, 2)}</pre>`;
   }
 }
 
-export interface IUser {
+function randomHouse(): House {
+  const randNum = Math.floor(Math.random() * 2);
+  switch(randNum) {
+    case 0: {
+      return House.GRYFFINDOR;
+    }
+    case 1: {
+      return House.SLYTHERIN;
+    }
+    default: {
+      return House.GRYFFINDOR
+    }
+  }
+}
+
+const MOCK_STUDENT_FACTORY = (index: number): IStudent => {
+  return {
+    id: `generated-student-${index}`,
+    firstName: "Generated",
+    lastName: "Student",
+    house: randomHouse(),
+    points: 32,
+    tags: [],
+    gender: ''
+  }
+};
+
+export interface IStudent {
   id: string;
   firstName: string;
   lastName: string;
-  isAdmin: boolean;
+  house: House;
+  points: number;
+  tags: string[];
+  gender: string;
 }
 
-export const MOCK_USERS: IUser[] = [
+export const MOCK_STUDENTS: IStudent[] = [
   {
-    id: "user1",
+    id: "harry_potter",
+    firstName: "Harry",
+    lastName: "Potter",
+    house: House.GRYFFINDOR,
+    points: 256,
+    tags: [],
+    gender: 'male'
+  },
+  {
+    id: "ron_weasley",
     firstName: "Ron",
-    lastName: "WEASLEYYYYYYY",
-    isAdmin: false
+    lastName: "Weasley",
+    house: House.GRYFFINDOR,
+    points: 64,
+    tags: [],
+    gender: 'male'
+  },
+  {
+    id: "draco_malfoy",
+    firstName: "Draco",
+    lastName: "Malfoy",
+    house: House.SLYTHERIN,
+    points: 128,
+    tags: [],
+    gender: 'male'
   }
 ];
 
-const users = mocklify<IUser>()
-  .add(5, MOCK_USERS)
-  .transform(override({ isAdmin: true }))
-  .getAll();
+const students = mocklify<IStudent>()
+    .addAll(MOCK_STUDENTS)
+    .generate(7, MOCK_STUDENT_FACTORY)
+    .transform(
+        omit(['gender']),
+        where(isGryffindor, modify(student => (student.points += 1000))),
+        where(isSlytherin, override({ points: 0 })),
+        where( isHarry, override({ points: 9999, isAdmin: true })),
+        override({tags: ['student']}),
+        modify((student, index, allStudents) => {
+          student.id = `student_${index}`;
+        })
+    )
+    .getAll();
 
-prettyPrintUsers(users);
+prettyPrintStudents(students);
