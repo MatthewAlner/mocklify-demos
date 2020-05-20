e8edbf9634239469e64f872ae6c80cf9c3b6c3fa
Matthew Alner <matthewalner@googlemail.com>
2020-05-20 13:06:59 +0100

    feat: add simple example

---
+++ simple/index.ts
@@ -0,0 +1,31 @@
+import { mocklify, override } from 'mocklify';
+
+function prettyPrintUsers(users: IUser[]) {
+    const appDiv: HTMLElement = document.getElementById('app');
+    appDiv.innerHTML = `<pre>${JSON.stringify(users, null, 2)}</pre>`;
+}
+
+export interface IUser {
+    id: string;
+    firstName: string;
+    lastName: string;
+    isAdmin: boolean;
+}
+
+export const MOCK_USERS: IUser[] = [
+    {
+        id: 'user1',
+        firstName: 'Harry',
+        lastName: 'Potter',
+        isAdmin: false
+    }
+]
+
+const users = mocklify<IUser>()
+    .add(5, MOCK_USERS)
+    .transform(
+        override({isAdmin: true}),
+    )
+    .getAll();
+
+prettyPrintUsers(users);
